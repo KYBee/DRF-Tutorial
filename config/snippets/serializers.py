da8ca815cf045563939d0445c1beb9831fa56c09
KYBee <kyb3634@naver.com>
2021-09-18 19:54:54 +0900

    feat : user ModelSerializer

--- config/snippets/serializers.py
+++ config/snippets/serializers.py
@@ -3,23 +3,30 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 #TODO Serializers.py 에는 항상 create와 update 메소드가 있어야 한다.
-class SnippetSerializer(serializers.Serializers):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template' : 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default="python")
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default="friendly")
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template' : 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default="python")
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default="friendly")
 
-    def create(self, validated_data):
-        return Snippet.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Snippet.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        #get(A, B) => A 에 해당하는 값이 없으면 B를 고른다.
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         #get(A, B) => A 에 해당하는 값이 없으면 B를 고른다.
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
+
+
+#ModelSerializer 사용하면 자동으로 만들어줌
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
